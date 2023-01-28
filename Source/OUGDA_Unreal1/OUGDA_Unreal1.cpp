c244e3d2a94164e45b867513edc150dc421f9574
Natalie <natalie.soltis@outlook.com>
2023-01-28 02:01:19 -0500

    initial files

---
+++ Source/OUGDA_Unreal1/OUGDA_Unreal1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OUGDA_Unreal1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OUGDA_Unreal1, "OUGDA_Unreal1" );
