9d96cba3668225b9a5ec9163c613fa7434f5b03e
Natalie <natalie.soltis@outlook.com>
2023-01-28 12:37:43 -0500

    start work

--- Source/OUGDA_Unreal1/Player/MainCharacter.cpp
+++ Source/OUGDA_Unreal1/Player/MainCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Player/MainCharacter.h"
+#include "MainCharacter.h"
 
 // Sets default values
 AMainCharacter::AMainCharacter()
